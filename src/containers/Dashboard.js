7a130c210b1e851e5f180454fbb74320f08609bc
aeonshad <shadaeon@gmail.com>
2024-02-06 17:23:43 +0100

    Fix fourth error

--- src/containers/Dashboard.js
+++ src/containers/Dashboard.js
@@ -86,6 +86,8 @@ export default class {
   }
 
   handleEditTicket(e, bill, bills) {
+    //Fourth error
+    e.stopImmediatePropagation();
     if (this.counter === undefined || this.id !== bill.id) this.counter = 0
     if (this.id === undefined || this.id !== bill.id) this.id = bill.id
     if (this.counter % 2 === 0) {
@@ -131,6 +133,8 @@ export default class {
   }
 
   handleShowTickets(e, bills, index) {
+    //Fourth error
+    e.stopImmediatePropagation();
     if (this.counter === undefined || this.index !== index) this.counter = 0
     if (this.index === undefined || this.index !== index) this.index = index
     if (this.counter % 2 === 0) {
